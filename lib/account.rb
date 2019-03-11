eed2743f7b7c2f237e7b9e7d04befe2895554246
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-03-11 13:18:56 +0000

    feat: transactions are being recorded on deposit

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,18 @@
+require 'date_stamp_module'
+
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
+  include DateStamp
 
   def initialize(balance)
     @balance = balance
+    @transactions = []
   end
 
   def deposit(monies)
     @balance += monies
+    @transactions << [date, monies, 0, @balance]
   end
 
   def withdraw(monies)
