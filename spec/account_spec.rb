eb3f2baef5a968d148c073e9304f1b154eac21ab
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-03-11 13:16:44 +0000

    test: upon deposit, date, credit, debit amount and balance are recorded

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,8 @@ require './lib/account'
 RSpec.describe 'Account' do
   let(:my_account) { Account.new(0) }
   let(:monies) { 100 }
+  let(:day) { '11/03/2019' }
+  let(:default) { 0 }
 
   it 'user can access the account' do
     expect(my_account.balance).to eq 0
@@ -27,5 +29,15 @@ RSpec.describe 'Account' do
 
   end
 
+  context 'transaction information' do
+
+    it 'upon deposit date, credit, debit amount and balance are recorded' do
+      allow(my_account).to receive(:date).and_return(day)
+      my_account.deposit(monies)
+      expect(my_account.transactions).to eq ([[day, monies, default, my_account.balance]])
+    end
+
+
+  end
 
 end
