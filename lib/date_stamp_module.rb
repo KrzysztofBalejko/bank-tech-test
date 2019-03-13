66f26a5cf1049c78b65358a58c1208b136cb2482
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-03-13 08:30:51 +0000

    refactor: date method is now shorter

--- lib/date_stamp_module.rb
+++ lib/date_stamp_module.rb
@@ -1,14 +1,7 @@
 module DateStamp
 
   def date
-    arr = [] << Time.new
-    str = arr.join("").slice!(0, 10).tr!('-', '/')
-    date_helper(str)
-  end
-
-  def date_helper(str)
-    order = [] << str.slice(8, 10) << str.slice(4, 4) << str.slice(0, 4)
-    order.join("")
+    Time.new.strftime("%d/%m/%Y")
   end
 
 end
